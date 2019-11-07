8ed0f83c8d7e90754ef80eb11f677880835562bf
James <bucklandj2@cardiff.ac.uk>
2019-11-07 15:15:03 +0000

    updated readme

--- frontend/src/components/MessageInput.js
+++ frontend/src/components/MessageInput.js
@@ -2,7 +2,7 @@ import Chat from './Chat';
 import React, { Component } from 'react';
 import getWeather from '../api/weatherApi';
 import { saveChatHistory, sendMessage } from '../api/chatApi';
-import { geolocated } from "react-geolocated";
+import { geolocated } from 'react-geolocated';
 import { Formik, Form, Field, ErrorMessage, } from 'formik';
 
 class MessageInput extends Component {
@@ -41,66 +41,66 @@ class MessageInput extends Component {
 				/>
 
 				<Formik
-				initialValues={{ChatMessage: '', Species: '', Abundance: '' }}
-				validate= {
-					values => {
-						let errors = {};
-						if ( values.file && values.file.type !== 'image/png') {
-							errors.file = 'File must be a .png or .jpg'
+					initialValues={{ChatMessage: '', Species: '', Abundance: '' }}
+					validate= {
+						values => {
+							let errors = {};
+							if ( values.file && values.file.type !== 'image/png') {
+								errors.file = 'File must be a .png or .jpg'
+							}
+							if ( ! values.ChatMessage) {
+								errors.ChatMessage = 'Required'
+							}
+							if( ! values.Species ) {
+								errors.Species = 'Required'
+							}
+							if( ! values.Abundance) {
+								errors.Abundance = ' Required'
+							}
+							return errors;
 						}
-						if ( ! values.ChatMessage) {
-							errors.ChatMessage = 'Required'
-						}
-						if( ! values.Species ) {
-							errors.Species = 'Required'
-						}
-						if( ! values.Abundance) {
-							errors.Abundance = ' Required'
-						}
-						return errors;
 					}
-				}
 
-				onSubmit = {(values, { setSubmitting, resetForm }) => {
-					setTimeout(() => {
-						// Adds addition information before passing the value
-						Object.assign(values, { 
-							ChatTimeStamp : new Date().toISOString().slice(0, 19).replace('T', ' '),
-							Username: this.props.username,
-							Longitude: this.props.coords.longitude,
-							Latitude: this.props.coords.latitude,
-							Weather: this.state.weather,
-						})
-						setSubmitting(false);
-						saveChatHistory(values);
-						sendMessage(values);
-					}, 400);
-					// Resets to initial values
-					resetForm({});
-				}}
+					onSubmit = {(values, { setSubmitting, resetForm }) => {
+						setTimeout(() => {
+							// Adds addition information before passing the value
+							Object.assign(values, { 
+								ChatTimeStamp : new Date().toISOString().slice(0, 19).replace('T', ' '),
+								Username: this.props.username,
+								Longitude: this.props.coords.longitude,
+								Latitude: this.props.coords.latitude,
+								Weather: this.state.weather,
+							})
+							setSubmitting(false);
+							saveChatHistory(values);
+							sendMessage(values);
+						}, 400);
+						// Resets to initial values
+						resetForm({});
+					}}
 				>
 
-				{({ isSubmitting, setFieldValue }) => (
-					<Form className="main">
-						<Field type="text" name="ChatMessage" placeholder = "Enter Title"></Field>
-						<ErrorMessage name="ChatMessage" component="div" /> 
-						<Field type="text" name="Species" placeholder="Enter Species"/>
-						<ErrorMessage name="Species" component="div" /> 
-						<Field type= "number" name="Abundance" placeholder="Enter Abundance"/>
-						<ErrorMessage name="Abundance" component="div" /> 
-						<button type="submit" disabled={isSubmitting}>
+					{({ isSubmitting }) => (
+						<Form className='main'>
+							<Field type='text' name='ChatMessage' placeholder = 'Enter Title'></Field>
+							<ErrorMessage name='ChatMessage' component='div' /> 
+							<Field type='text' name='Species' placeholder='Enter Species'/>
+							<ErrorMessage name='Species' component='div' /> 
+							<Field type= 'number' name='Abundance' placeholder='Enter Abundance'/>
+							<ErrorMessage name='Abundance' component='div' /> 
+							<button type='submit' disabled={isSubmitting}>
 						Submit
-						</button>
-					</Form>
-				)}
+							</button>
+						</Form>
+					)}
 				</Formik>
 			</div>
 		)
 	}
 }
 export default geolocated({
-    positionOptions: {
-        enableHighAccuracy: false,
-    },
-    userDecisionTimeout: null,
+	positionOptions: {
+		enableHighAccuracy: false,
+	},
+	userDecisionTimeout: null,
 })(MessageInput);
